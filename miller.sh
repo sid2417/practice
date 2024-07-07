1bce7c3b8979b6bd0b4590516365ada7d407ce42
sid2417 <siddhartha.java9398@gmail.com>
2024-07-07 21:25:52 +0530

    Conversations between team

---
+++ miller.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Miller: Hi SA team, Iam sorry"
+echo "SA-TEAM: No miller, its not only your mistake, its a team mistake"
+echo "Miller: But I need to finish the game, but I couldn't, sorry once again team"
+echo "SA-TEAMNo worries Miller, we played very well, this time I beleive luck is not in favaour to us.."
