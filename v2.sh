481f81924e068fc965f10a9ec8dc81d8ca18f7bb
sid2417 <siddhartha.java9398@gmail.com>
2024-07-07 21:50:10 +0530

    Team conversation

--- v2.sh
+++ v2.sh
@@ -3,15 +3,15 @@
 # we are passing variable in the form of argument manner
 
 echo " please enter your captain name "
-read $VARIABLE1
+read VARIABLE1
 
 
 echo " please enter your team name "
-read $VARIABLE2
+read VARIABLE2
 
 
 
-ehco "$VARIABLE1 : Hi team, we are entered into the finals, congrats all of you"
+echo "$VARIABLE1 : Hi team, we are entered into the finals, congrats all of you"
 echo "$VARIABLE2 : Thanks captain, we all are proud of you as a captain"
 echo "$VARIABLE1 : Thankyou guys..we will celebrate this night with our victory"
 echo "$VARIABLE2 : Yes captain.. we also waiting for that war.. to enjoy the victory"
