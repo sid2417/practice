41450e89dca84d7403b49a5dd9079d0cbc33aa7a
sid2417 <siddhartha.java9398@gmail.com>
2024-07-07 21:40:41 +0530

    Team conversation

--- variable2.sh
+++ variable2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 VARIABLE1=captain
 VARIABLE2=team
 
-echo "$VARIABLE1 : Hi team, we are entered into the finals, congrats all of you"
+ehco "$VARIABLE1 : Hi team, we are entered into the finals, congrats all of you"
 echo "$VARIABLE2 : Thanks captain, we all are proud of you as a captain"
 echo "$VARIABLE1 : Thankyou guys..we will celebrate this night with our victory"
 echo "$VARIABLE2 : Yes captain.. we also waiting for that war.. to enjoy the victory"
