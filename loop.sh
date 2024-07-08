c8dcc73d901cd8f72274c9a88f2fba9eac9730a1
sid2417 <siddhartha.java9398@gmail.com>
2024-07-09 02:56:17 +0530

    Installing softwares

--- loop.sh
+++ loop.sh
@@ -10,16 +10,15 @@ VALIDATE( ){
 }
 
 
-for i in {$@}
+for i in $@
 do 
-    echo "Installing Package Name : $i"
-    dnf list install $i
-    if [ $i -eq 0 ]
+    echo "Package Name : $i"
+    dnf list installed $i
+    if [ $? -eq 0 ]
     then
-        echo "This is already Installed"
+        echo "This is already Installed....Just SKIPPING"
     else
         dnf install $i -y
         VALIDATE $? "Current Package $i is :"
     fi
-
 done
