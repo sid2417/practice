9c47aa664ed84595d85a97e019083011ce5dca63
sid2417 <siddhartha.java9398@gmail.com>
2024-07-10 01:08:18 +0530

    Installing DB server

--- db.sh
+++ db.sh
@@ -7,7 +7,7 @@ N='\e[0m'
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 FILENAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$FILENAME-$TIMESTAMP
+LOGFILE=/tmp/$FILENAME-$TIMESTAMP.log
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
@@ -17,6 +17,9 @@ then
 else
     echo -e "$G You Have already SUDO ACCESS $N"
 fi
+
+echo -e "$Y Please Enter Your DB Password: $N"
+read -s Mysql_Password
 
 VALIDATE(){
      if [ $1 -ne 0 ]
@@ -37,5 +40,7 @@ VALIDATE $? "Enabling Mysql is :"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting Mysql is :"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+
+mysql_secure_installation --set-root-pass $Mysql_Password &>>$LOGFILE
 VALIDATE $? "Setup Mysql Password is :"
